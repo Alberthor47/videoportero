a5d93315adf666fd0348b93ec850ff53bac0f156
Alberto Camarena <alberto@camarena.me>
2024-08-13 18:07:48 -0600

    V1 of the front

---
+++ readme.md
@@ -0,0 +1,16 @@
+# VideoPortero
+
+VideoPortero is a project that aims to create a video intercom system using modern technologies. This system allows users to communicate with visitors at shop doorstep through video and audio, enhancing security and convenience.
+
+## Installation
+
+1. Clone the repository: `git clone https://github.com/<your-username>/videoportero.git`
+2. Navigate to the project directory: `cd videoportero`
+3. Install the necessary dependencies: `npm install`
+4. Make sure you have a `.env` file in the root of the project with the following environment variables:
+   - `AGORA_APP_ID`: The App ID of the Agora project.
+   - `BASE_URL`: The base URL of the server.
+
+## Usage
+
+First make sure to buil the project by running `npm run build`. Then you can start the app by running `npm run start:dev`.
---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const path = require("path");
+const dotenv = require('dotenv');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: "./index.js",
+    output: {
+        filename: "index.js",
+        path: path.resolve(__dirname, "./dist"),
+    },
+    devServer: {
+        static: {
+            directory: path.join(__dirname, './'), 
+        },
+        compress: true,
+        port: 9000,
+    },
+    mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
+    plugins: [
+        new webpack.DefinePlugin({
+            'process.env': JSON.stringify(dotenv.config().parsed || {})
+        }),
+        new HtmlWebpackPlugin({
+          template: './index.html'
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: ["@babel/preset-env"],
+                    },
+                },
+            },
+            {
+                test: /\.css$/,
+                use: ["style-loader", "css-loader"],
+            },
+        ],
+    },
+};
